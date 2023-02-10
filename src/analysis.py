487733e127d279c182f601e1d7ec05d8e81e6f3f
Evanskorir <104888028+Evanskorir@users.noreply.github.com>
2023-02-10 12:02:38 +0100

    - added a method to incorporate the indicators to execute the plots and clustering separately

--- src/analysis.py
+++ src/analysis.py
@@ -5,14 +5,14 @@ from sklearn.decomposition import PCA
 from src.data_transformer import Contacts
 from src.d2pca import D2PCA
 from src.hierarchical import Hierarchical
+from src.indicators import Indicators
 
 
 class Analysis:
-    def __init__(self, pca_data, data_tr: Contacts, img_prefix, threshold,
+    def __init__(self, data_tr: Contacts, img_prefix, threshold,
                  n_components: int = 4,
                  dim_red: str = None, distance: str = "euclidean"):
         self.data_tr = data_tr
-        self.pca_data = pca_data
         self.dim_red = dim_red
         self.img_prefix = img_prefix
         self.threshold = threshold
@@ -20,6 +20,8 @@ class Analysis:
         self.n_components = n_components
         if dim_red is not None:
             self.apply_pca()
+            self.contacts_apply_indicators()
+
         self.hierarchical = Hierarchical(data_transformer=self.data_tr,
                                          country_names=self.data_tr.country_names,
                                          img_prefix=self.img_prefix,
@@ -30,41 +32,51 @@ class Analysis:
             pca = PCA(n_components=self.n_components)
             pca.fit(self.data_tr.data_clustering)
             data_pcaa = pca.transform(self.data_tr.data_clustering)  # dim 4
+            ind = Indicators(data_tr=self.data_tr, country_names=self.data_tr.country_names)
 
             # integrate indicators
-            data_pca = np.append(data_pcaa, self.pca_data, axis=1)  # dim 8
+            data_pca = np.append(data_pcaa, ind.pca_data, axis=1)  # dim 8
             print("Explained variance ratios pca:",
                   pca.explained_variance_ratio_,
                   "->", sum(pca.explained_variance_ratio_))
         elif self.dim_red == "2DPCA":
             data_dpca = D2PCA(country_names=self.data_tr.country_names, data_tr=self.data_tr)
             data_dpca.apply_dpca()
-            data_pcaa = data_dpca.pca_reduced  # dim 4
-
-            # integrate indicators to the contact matrices
-            data_pca = np.append(data_pcaa, self.pca_data, axis=1)  # dim 8
+            data_pca = data_dpca.pca_reduced  # dim 4
         else:
             raise Exception("Provide a type for dimensionality reduction.")
         self.data_tr.data_clustering = data_pca
 
+    def contacts_apply_indicators(self):
+        """
+        This part incorporates socioeconomic indicators from the indicators file.
+        2DPCA approach is used.
+        """
+        if self.dim_red == "2DPCA":
+            data_2 = D2PCA(country_names=self.data_tr.country_names, data_tr=self.data_tr)
+            data_2.apply_dpca()
+            data_p = data_2.pca_reduced  # dim (32, 4)
+            # incorporate socioeconomic indicators to the contact matrix using the same approach
+            ind = Indicators(data_tr=self.data_tr, country_names=self.data_tr.country_names)
+            ind.pca_apply()
+            data_pca2 = np.append(data_p, ind.pca_data, axis=1)  # dim (32, 8)
+        else:
+            raise Exception("The indicators could not be incorporated.")
+        self.data_tr.data_clustering = data_pca2
 
-def kenya_contacts(data_tr):
-    age_group = ["0-4", "5-14", "15-19", "20-24", "25-64", "65+"]
-    plt.figure(figsize=(13, 12))
-    full = plt.imshow(data_tr.full_contacts['Kenya']['contact_full'],
-                      cmap='jet', alpha=.9, interpolation="nearest", vmin=0, vmax=16)
 
-    ticks = np.arange(0, 6)
-    cbar = plt.colorbar(full, shrink=0.87)
-    tick_font_size = 40
-    cbar.ax.tick_params(labelsize=tick_font_size)
-    plt.xticks(ticks=ticks, labels=age_group, rotation=90, fontsize=32)
-    plt.yticks(ticks=ticks, labels=age_group, rotation=0, fontsize=32)
-    plt.gca().invert_yaxis()
-    # plt.xlabel("Age", fontsize=28)
-    # plt.ylabel("Age", fontsize=28)
-    # plt.title("Other", fontsize=40)
-    # plt.show()
-    # here you create plots for contacts at home, school, work, other, and all
-    # plt.savefig("../plots/" + "All.pdf")
-    # by manipulating contact in data_transformer.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
